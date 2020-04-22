76b3a306415f9c233e924f47beca61b28831a7ec
dytq <taariq.deda@gmail.com>
2020-04-22 20:25:58 +0200

    plus simple encore

--- .script.sh
+++ .script.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-mkdir bin 2> /dev/null ;
-mkdir obj 2> /dev/null ;
-mkdir obj/gestion_fichier 2> /dev/null;
-mkdir obj/generation 2> /dev/null ;
-mkdir obj/interface 2>/dev/null ;
-mkdir obj/chiffrement 2> /dev/null ;
-mkdir obj/dechiffrement 2> /dev/null ;
-mkdir obj/signature 2> /dev/null ;
-mkdir obj/calcul 2> /dev/null
+mkdir bin obj obj/gestion_fichier  obj/generation obj/interface obj/chiffrement obj/dechiffrement obj/signature obj/calcul 2> /dev/null
