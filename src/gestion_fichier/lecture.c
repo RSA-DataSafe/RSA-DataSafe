e03310b3cbbd6cbcadb979d7b0031bb836f37996
dytq <taariq.deda@gmail.com>
2020-04-23 16:18:35 +0200

    Ajout lecture clée pub/priv + lire fichier

--- src/gestion_fichier/lecture.c
+++ src/gestion_fichier/lecture.c
@@ -36,10 +36,79 @@ int chercher_utilisateur(char *email, char *mdp) {
 												return 1;
 										}
 										else {
-												break; // Retourner le mot de passe faux, on ne vérifie pas le reste 
+												break; // on ne vérifie pas le reste car le mot de passe est faux 
 										}
 						}
 		}
 		fclose(fichier);	
 		return 0;
+}
+
+int recupere_cle_publique(char * email, char * mdp, cle_publique * publique) {
+		if(!chercher_utilisateur(email,mdp) == 0) {
+				char * chemin = malloc(sizeof(char)*MAX_CARACT + sizeof("rsa/") + sizeof("cle_privee"));
+				strcat(chemin,"rsa/");
+				strcat(chemin,email);
+				strcat(chemin,"/cle_privee");
+
+				char * res = lire_fichier(chemin);
+				
+				if(res[0] == -1) {
+						return ERR_LECT;
+				}
+				// init cle publique avec res
+				free(chemin);
+				free(res);
+				return 0;	
+		}
+		return ERR_LECT;
+}
+
+int recupere_cle_privee(char * email, char * mdp, cle_prive * prive) {
+		if(!chercher_utilisateur(email,mdp) == 0) {
+					char * chemin = malloc(sizeof(char) * MAX_CARACT + sizeof("rsa/") + sizeof("/cle_pub"));
+					char * res = lire_fichier(chemin);
+					
+					strcat(chemin,"rsa/");
+					strcat(chemin,email);
+					strcat(chemin,"cle_pub");
+
+					if(res[0] == -1) {
+								return ERR_LECT;
+					}
+					//init cle privée avec res
+					free(chemin);
+					free(res);
+					return 0;
+		}
+		return ERR_LECT;
+}
+
+int lire_boite(char *email, boite * b) {
+	 char * chemin = malloc(sizeof("rsa/") + sizeof(email)+ sizeof("/boiteN"));
+	 strcat(chemin,email);	
+	 strcat(chemin,"boite");
+	 // cat le num
+	 // obtenir le numéro de la boite
+	 // b.boite = lire_fichier(chemin); 
+	 free(chemin);	
+	 return 0;	
+}
+
+char *lire_fichier(char * chemin) {
+		char * res = malloc(sizeof(char)*2000); // longueur d'une clée à changer je ne sais pas si bin ou hexa ? 
+		FILE * fichier = NULL;
+		fichier = fopen(chemin,"r");
+		if(fichier == NULL) {
+				res[0] = -1;
+				return res;
+		}
+		
+		char c;
+		for(int i = 0; (c = fgetc(fichier)) != EOF; i++) {
+					res[i] = c;
+		}
+
+		fclose(fichier);
+		return res;
 }
