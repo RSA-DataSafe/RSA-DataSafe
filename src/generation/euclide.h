f78550bff23d90bccc12736b0eecb5f70232657a
Lyne <55532663+LyndaHadjab@users.noreply.github.com>
2020-04-25 00:23:52 +0200

    Update euclide.h

--- src/generation/euclide.h
+++ src/generation/euclide.h
@@ -4,10 +4,11 @@
 
 /**
  * Euclide étendu : permet le calcul de u*phi(n) + v*e = pgcd(phi(n),e) 
- * @param u : résultat
- * @param v : résultat
+ * @param u_1 : résultat
+ * @param v_1 : résultat
  * @param phi_n : le phi(n) de RSA 
  * @param e : clé publique de RSA 
+ *@param pgcd :pgcd(phi_n,e)
  * */
 void euclide_etendu(mpz_t u_1, mpz_t v_1, mpz_t phi_n, mpz_t e,mpz_t pgcd);
 
