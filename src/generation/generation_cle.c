61bfaeb5a9be1661daac4affbe5013a456d07e76
LASSALryma <43700917+LASSALryma@users.noreply.github.com>
2020-04-28 00:38:29 +0200

    Update generation_cle.c

--- src/generation/generation_cle.c
+++ src/generation/generation_cle.c
@@ -5,113 +5,100 @@
 #include "euclide.h"
 #include <unistd.h>
 #include <errno.h>
+#include <sys/wait.h> 
 #include <assert.h>
 #include <sys/types.h>
 #include <time.h>
-#include "primalite.h"
+#include "miller-rabin.h"
 
-
+/**
+ * Procédure qui permet de calculer phi(n) = (p -1) * (q-1)
+ * @param p : p de RSA
+ * @param q : q de RSA
+ * @param phi_n : résultat, calculer par la fonction 
+ * */
 void calculPhi(mpz_t p, mpz_t q, mpz_t phi_n){
-    mpz_t tmp1,tmp2;
-    mpz_init(tmp1);
-    mpz_init(tmp2);
- //pour ne pas perdre la valeur de p et q 
-	mpz_sub_ui(tmp1,p,1);
-	mpz_sub_ui(tmp2,q,1);
+	//p-1
+	mpz_sub_ui(p,p,1);
+	//p-1
+	mpz_sub_ui(q,q,1);
 	//phi(n)=(p-1)(q-1)
-	mpz_mul(phi_n,tmp1,tmp2);	
-	mpz_clear(tmp2);
-	mpz_clear(tmp1);
+	mpz_mul(phi_n,p,q);	
 }
 
+/**
+ * Calcul de la clé publique (n,e) tel que pgcd(e,phi(n)) = 1 et e < phi
+ * @param publique : clé publique
+ * */
+void calculerE(cle_publique *publique){
+	//valeur aléatoire ,servira pour le choix de la valeur de e 
+	srand(time(NULL));
+	int tmp=rand()%4;
 
-void calculerE(cle_publique *publique,mpz_t phi){//ici on a aussi oublier le paramtre 
-	
-  time_t t; 
-  int seed;
-  seed = (int) time(&t);
-  gmp_randstate_t etat ;
-  gmp_randinit_default(etat);
-  gmp_randseed_ui(etat, seed);
-  mpz_t gcd;
-  mpz_init(gcd);
-  for(;;)
-  {
-  	   mpz_urandomm(publique->e, etat,phi);//parce que on a oublié de mettre le paramètre phi_n
-  	   mpz_gcd(gcd,publique->e,phi); 
-  	   if(mpz_cmp_ui(gcd,1)==0 && mpz_cmp_ui(publique->e,0)!=0 && mpz_cmp_ui(publique->e,1)!=0)
-  	   {
-           break;
-  	   }
+	int val[5]={3,5,17,257,65537};
 
-  }
-
+	// valeur de e
+	mpz_set_ui(publique->e,val[tmp]);
 		
 	}
 
-void calculerD(cle_prive *prive, cle_publique *publique,mpz_t phi){ //on a oublier le paramètre phi
-    mpz_t u;
-	mpz_init(u);
-	mpz_t v;
-	mpz_init(v);
-	euclide_etendu(u, v, phi, publique->e);
-	
-	if(mpz_cmp_ui(v, 0) < 0) {
-		while( mpz_cmp_ui(v, 0) < 0) {
-			mpz_add(v, v, phi);
-		}
-	}
-	mpz_set(prive->d, v);
-	mpz_clear(u);
-	mpz_clear(v);
+/**
+ * Calcul de l'exposant privé "d" tel que  (e*d = 1 mod phi)
+ * @param prive : clé privé
+ * @param publique : clé publique
+ * */
+void calculerD(cle_prive *prive, cle_publique *publique){
+	mpz_t u,v;
+	mpz_inits(u,v,NULL);
+	euclide_etendu(u , v, prive->n, publique->e);
+	mpz_set(prive->d,v);
+	mpz_set(prive->n,publique->n);
+	mpz_clears(u,v,NULL);
 }
 
 void GenererPQRSA(mpz_t p, mpz_t q, mpz_t n, int taille){
+
+		do{
+			bbs(p,taille);
+			}while(mpz_cmp_ui(p,0)==0 || Miller_Rabin(p,4)!=1);
+				mpz_set(n,p);
+
+		do{
+			bbs(q,taille);
+
+		}while(mpz_cmp_ui(q,0)==0 || Miller_Rabin(q,4)!=1|| mpz_cmp(p,q)==0);
+			mpz_mul(n,n,q);
 	
-	mpz_t ite;
-	mpz_init(ite);
-	mpz_set_ui(ite,1);
-     for(;;)
-	{
-		bbs(p,taille/2);
-		mpz_out_str(0,10,p);
-		printf("\n\n");
-     if((miller_rabbin(p,ite)==1))
-      {
-	       break;
-      }
-    }
-   for(;;)
-		{
-			bbs(q,taille/2);
-			if(miller_rabbin(q,ite)==1 &&  mpz_cmp(q,p)!=0)
-			{
-				break;
-			}
-		
 
-	}
 
-	mpz_mul(n,p,q);
-	mpz_clear(ite);
-
-	}
-void genere_cle(cle_publique *publique, cle_prive *prive, int taille){//taille c est 2048
    
-	mpz_t p;
-	mpz_init(p);
-	mpz_t q;
-	mpz_init(q);
-	mpz_t n;
-	mpz_init(n);
-	mpz_t phi;
-	mpz_init(phi);
-    GenererPQRSA(p,q,n,taille);
-	calculPhi(p,q,phi);
-	calculerE(publique,phi);
-    calculerD(prive,publique,phi);
-    mpz_clear(p);
-    mpz_clear(q);
-    mpz_clear(phi);
-    mpz_clear(n);
+	}
+void genere_cle(cle_publique *publique, cle_prive *prive, int taille){
+
+	mpz_t p,q,n,phi_n;
+	mpz_inits(p,q,n,phi_n, NULL);
+	//génération de p et q
+	GenererPQRSA(p,q,n,taille);
+	mpz_mul(n,p,q);
+	//calcul de phi(n)
+	calculPhi(p,q,phi_n);
+
+	//mpz_set(prive->n,phi_n);
+
+	// choix de l'exposant pulique e
+	calculerE(publique);
+
+	mpz_set(publique->n,n);
+
+	//pour avoir la valeur de phi(n) qui nous servira pour le calcul de d
+	mpz_set(prive->n,phi_n);
+	
+
+	calculerD(prive, publique);
+	
+
+	mpz_clears(p,q,n,phi_n, NULL);
 }
+
+
+
