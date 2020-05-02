e2d1426e08dce0c04b7beeb849e87a928c1becf0
dytq <taariq.deda@gmail.com>
2020-05-02 13:04:46 +0200

    init conversion

---
+++ src/gestion_fichier/conversion.h
@@ -0,0 +1,20 @@
+#ifndef _conversion_h
+#define _conversion_h
+
+/**
+ * Permet de convertir une cha î ne de caract è res en un message
+sous forme de nombre
+ * @param m : la cha î ne de caract è res
+ * @return le message sous forme de nombre
+**/
+message * conversion_char_mpz ( char * chaine ) ;
+
+/**
+ * Permet de convertir un message sous forme de nombre en une
+cha î ne de caract è res
+ * @param m : le message sous forme de nombre
+ * @return la cha î ne de caract è res
+**/
+char * conversion_mpz_char ( message * m ) ;
+
+#endif // !_conversion_h
