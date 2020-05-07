55e2869d19c63aef7f54825bee87f2986e373743
Sholde <exshode@gmail.com>
2020-05-07 21:11:46 +0200

    fonction oaep_1

--- src/dechiffrement/oaep_1.c
+++ src/dechiffrement/oaep_1.c
@@ -10,30 +10,38 @@
 
 
 block *oaep_1(block *b) {
-    mpz_t x ,y, m , r ;
-    mpz_init(x);
-    mpz_init(y);
-    mpz_init(r);
+    // init
+    mpz_t m;
     mpz_init(m);
 
+    mpz_t r;
+    mpz_init(r);
 
-    block *retour ;
-    retour -> tab = malloc(sizeof(int)*b-> nb_block);
-    retour -> nb_block = b-> nb_block;
-    
-   
-    for(int i = 0; i < b->nb_block; i++)
-    {
-        extraction(mpz_t x, mpz_t y, b->tab[i]);
-        oaep_block(x,y,m,r);
-        mpz_set(retour-> tab[i], m);
-    
+    mpz_t x;
+    mpz_init(x);
+
+    mpz_t y;
+    mpz_init(y);
+
+    mpz_t tmp;
+    mpz_init(tmp);
+
+    // Code
+    for(int i = 0; i < b->nb_block; i++) {
+        extraction(x, y, b->tab[i]);
+        oaep_block(x, y, m, r);
+        shift_gauche(x, 256);
+        mpz_set(b->tab[i], m);
+        mpz_add(b->tab[i], b->tab[i], r);
     }
 
-    mpz_clear(m);
+    // Clear
+    mpz_clear(tmp);
     mpz_clear(y);
-    mpz_clear(r);
+    mpz_clear(x);
+    mpz_clear(m);
     mpz_clear(r);
 
-    return retour;
+    // return
+    return b; // sert à rien mais bon ...
 }
