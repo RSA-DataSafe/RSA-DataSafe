43544d6a39f0f803caec70e85b7e03985bb6e764
Lyne <55532663+LyndaHadjab@users.noreply.github.com>
2020-05-07 19:11:10 +0200

    Update signature.c

--- src/signature/signature.c
+++ src/signature/signature.c
@@ -3,33 +3,23 @@
 #include "../calcul/calcul.h"
 
 void signer(mpz_t signature, message *mes, cle_prive *prive) {
-	//mpz_t hache;
-	
-	//mpz_init_set(hache, mess.nombre);
-	//mpz_init(signature);
-
 	sha3(mes,512);
-	
 	expo_mod(signature, mes->nombre, prive->d, prive->n);
 	
-	//mpz_clear(hache);
 }
 
 int verifie_signature(mpz_t chiffre, mpz_t signature, cle_publique *publique) {
-	//mpz_t hache;
+	
 	mpz_t signature_prime;
 	int res = 0;
 	message m;
 	mpz_init(m.nombre);
 	mpz_init(m.taille);
-
+	mpz_set(m.nombre,chiffre);
 	sha3(&m,512);
-
 	expo_mod(signature_prime, m.nombre, publique->e, publique->n);
 	res = mpz_cmp(signature_prime,signature);
-	
 	mpz_clear(signature_prime);
-	//mpz_clear(hache);
 
 	return res;
 }
