78f020d7dffd94a4e7d40d46de92052ddf54525e
lynda <hadjablynda99@gmail.com>
2020-05-07 19:00:39 +0200

    pddie

--- src/signature/signature.c
+++ src/signature/signature.c
@@ -2,33 +2,37 @@
 #include "sha3.h"
 #include "../calcul/calcul.h"
 
-void signer(mpz_t signature, message *message, cle_prive *prive) {
-	mpz_t hache;
+void signer(mpz_t signature, message *mes, cle_prive *prive) {
+	//mpz_t hache;
 	
-	mpz_init_set(hache, message);
-	mpz_init(signature);
+	//mpz_init_set(hache, mess.nombre);
+	//mpz_init(signature);
 
-	hacher(hache,512);
+	sha3(mes,512);
 	
-	expo_mod(signature, hache, prive->d, prive->n);
+	expo_mod(signature, mes->nombre, prive->d, prive->n);
 	
-	mpz_clear(hache);
+	//mpz_clear(hache);
 }
 
 int verifie_signature(mpz_t chiffre, mpz_t signature, cle_publique *publique) {
-	mpz_t hache;
+	//mpz_t hache;
 	mpz_t signature_prime;
 	int res = 0;
 
-	mpz_init_set(hache, chiffre);
-	
-	hacher(hache,512);
+	//mpz_init_set(hache, chiffre);
+	//mpz_init(chiffre);
+	message m;
+	mpz_init(m.nombre);
+	mpz_init(m.taille);
 
-	expo_mod(signature_prime, hache, publique->e, publique->n);
+	sha3(&m,512);
+
+	expo_mod(signature_prime, m.nombre, publique->e, publique->n);
 	res = mpz_cmp(signature_prime,signature);
 	
 	mpz_clear(signature_prime);
-	mpz_clear(hache);
+	//mpz_clear(hache);
 
 	return res;
 }
