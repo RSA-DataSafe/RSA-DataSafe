ab322f7288613911463a1f4db9e915bf542d7909
Lyne <55532663+LyndaHadjab@users.noreply.github.com>
2020-05-07 19:16:32 +0200

    Update signature.c

--- src/signature/signature.c
+++ src/signature/signature.c
@@ -16,6 +16,8 @@ int verifie_signature(mpz_t chiffre, mpz_t signature, cle_publique *publique) {
 	mpz_init(m.nombre);
 	mpz_init(m.taille);
 	mpz_set(m.nombre,chiffre);
+       int taille=mpz_sizeinbase(chiffre,10);
+	mpz_set_ui(m.taille,taille);
 	sha3(&m,512);
 	expo_mod(signature_prime, m.nombre, publique->e, publique->n);
 	res = mpz_cmp(signature_prime,signature);
