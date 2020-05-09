d89625bfd9cec856dc5cc8cae1365cd5540b4c82
Sholde <exshode@gmail.com>
2020-05-09 19:23:12 +0200

    conversion hexa mpz

--- src/gestion_fichier/conversion.c
+++ src/gestion_fichier/conversion.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <gmp.h>
 
 message * conversion_char_mpz( char * chaine ) {
 	char byte[8]; 	
@@ -60,4 +61,70 @@ char * conversion_mpz_char( message * m ) {
 	ret[mpz_get_ui(m->taille)/7] = '\0';
 	printf("%s\n", ret);
 	return ret;
+}
+
+message *conversion_hexa_mpz ( char *chaine ) {
+	int len_chaine = strlen(chaine);
+
+	message *m = malloc(sizeof(message));
+	mpz_init(m->nombre);
+	mpz_init(m->taille);
+
+	mpz_set_ui(m->taille, 4);
+	mpz_mul_ui(m->taille, m->taille, len_chaine);
+
+	for (int i = 0; i < len_chaine; i++)
+	{
+		int tmp = chaine[i];
+		if(tmp >= 48 && tmp <= 57) {
+			tmp -= 48;
+		}
+		else if (tmp >= 97 && tmp <= 102) {
+			tmp -= (97 - 10);
+		}
+		else {
+			exit(666);
+		}
+		mpz_mul_2exp(m->nombre, m->nombre, 4);
+		mpz_add_ui(m->nombre, m->nombre, tmp);
+	}
+	
+	return m;
+}
+
+char *conversion_mpz_hexa ( message *m ) {
+	mpz_t mpz_taille;
+	mpz_init(mpz_taille);
+	mpz_div_ui(mpz_taille, m->taille, 4);
+
+	int taille = mpz_get_ui(mpz_taille);
+
+	char *chaine = malloc(sizeof(char) * (taille + 1) );
+	chaine[taille] = '\0';
+
+	mpz_t mpz_char;
+	mpz_init(mpz_char);
+
+	mpz_t AND;
+	mpz_init(AND);
+	mpz_set_ui(AND, 15);
+
+	for (int i = 0; i < taille; i++)
+	{
+		mpz_tdiv_q_2exp(mpz_char, m->nombre, (taille - (i+1)) * 4);
+		mpz_and(mpz_char, mpz_char, AND);
+		char tmp = mpz_get_ui(mpz_char);
+		if(tmp > 15 || tmp < 0) {
+			exit(666);
+		}
+		if(tmp < 10) {
+			tmp += 48;
+		}
+		else {
+			tmp += 87;
+		}
+		chaine[i] = tmp;
+	}
+	
+	return chaine;
 }
