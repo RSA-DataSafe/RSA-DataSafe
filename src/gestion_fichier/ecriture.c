96ce1503d77eb1a6e3fc1c6f736aac5651f377bb
Lyne <55532663+LyndaHadjab@users.noreply.github.com>
2020-05-10 21:47:21 +0200

    Update ecriture.c

--- src/gestion_fichier/ecriture.c
+++ src/gestion_fichier/ecriture.c
@@ -1,5 +1,6 @@
 #include "ecriture.h"
-char nom[]="connexion.txt";
+char nom[]="connexion";
+
 int  nouvel_utilisateur(char *email , char *mdp)
 {
        int taille= strlen(email);
@@ -10,11 +11,12 @@ int  nouvel_utilisateur(char *email , char *mdp)
        strcpy(dossier,ch1);
        strcat(dossier,email);
        mkdir(dossier,S_IRWXU);
+
        strcat(dossier,"/");
        char dossier1[300];
        strcpy(dossier1,dossier);
        strcat(dossier,userfile1);
-       int res1;int res2;
+       int res1 = 0,res2 = 0;
        FILE *userf1=fopen(dossier,"a+");
        if(userf1!=NULL)
        {
@@ -24,7 +26,7 @@ int  nouvel_utilisateur(char *email , char *mdp)
        {
         printf("Erreur de création d fichier\n");
        }
-       char *userfile2=malloc((taille+20)*sizeof(char));
+       char *userfile2=malloc((taille+50)*sizeof(char));
        strcpy(userfile2,"Cles.txt");
        strcat(dossier1,userfile2);
        FILE *userf2=fopen(dossier1,"a+");
@@ -42,28 +44,22 @@ int  nouvel_utilisateur(char *email , char *mdp)
        strcat(dossier,"/");
        strcat(dossier,userfile2);
        userf2=fopen(dossier,"a+");
-       if(userf2!=NULL)
-       {
-        fclose(userf2);
-       }
-       else
-       {
-        printf("Erreur  de création du fichier\n");
-       }
+       
+       if(userf2!=NULL) fclose(userf2);
+       
+       else  printf("Erreur  de création du fichier\n");
+       
        strcpy(userfile2,"MessageIndesirable.txt");
        strcpy(dossier,ch1);
        strcat(dossier,email);
        strcat(dossier,"/");
        strcat(dossier,userfile2);
        userf2=fopen(dossier,"a+");
-       if(userf2!=NULL)
-       {
-        fclose(userf2);
-       }
-       else
-       {
-        printf("Erreur  de création du fichier\n");
-       }
+       if(userf2!=NULL) 
+                     fclose(userf2);
+       else      
+                  printf("Erreur  de création du fichier\n");
+
        strcpy(userfile2,"MessageInvisible.txt");
        strcpy(dossier,ch1);
        strcat(dossier,email);
@@ -82,23 +78,25 @@ int  nouvel_utilisateur(char *email , char *mdp)
        strcat(cm,nom);
        FILE *fichier=fopen(cm,"a");
        char ligne [300]; 
+       //char *line_buf = NULL;
       if(fichier!=NULL)
        {
-      while(fgets (ligne, sizeof ligne,fichier ) != NULL && strcmp(ligne,"")!=0) ;
-      res1=ecrire_fichier(cm,email);
-      res2=ecrire_fichier(cm,mdp);
-      if(res1!=0||res2!=0)
-      {
-        return ERR_ERCI;
-      }
-      fclose ( fichier );
-      return 0;
+            while(fgets (ligne, sizeof ligne,fichier ) != NULL && strcmp(ligne,"")) ;
+            res1=ecrire_fichier(cm,email);
+            res1=ecrire_fichier(cm,mdp);
+            if(res1!=0 || res2!=0)
+            {
+              return ERR_ERCI;
+            }
+            fclose ( fichier );
+            return 0;
        }
-       else
-       {
+      
+       
           return ERR_ERCI;
-       }
+       
 }
+
 int  Change_Email(char *email , char *mdp , char *newemail)
 {
   char nomtmp[]="test1.txt";
@@ -217,111 +215,36 @@ int  Change_MotDePasse (char*email , char *mdp , char* newmdp)
 }
 int  change_cle(informations * InfoUser,cle_publique * pub ,cle_prive * priv)
 {
-  char dossier[200];
-  strcpy(dossier,"rsa/");
-  strcat(dossier,InfoUser->email);
-  strcat(dossier,"/");
-  strcat(dossier,"Cles.txt");
-  char tmp[200];
-  strcpy(tmp,"rsa/");
-  strcat(tmp,InfoUser->email);
-  strcat(tmp,"/Cles1.txt");
-  FILE *fichier=fopen(dossier,"r");
-  FILE * clefile=fopen(tmp,"a+");  
-  char *d =malloc(2048*sizeof(char));
-  char *e =malloc(2048*sizeof(char));
-  char *n =malloc(2048*sizeof(char));
-  mpz_get_str(d,10,InfoUser->prive.d);
-  mpz_get_str(e,10,InfoUser->publique.e);
-  mpz_get_str(n,10,InfoUser->publique.n);
-  char *d1 =malloc(2048*sizeof(char));
-  char *e1 =malloc(2048*sizeof(char));
-  char *n1 =malloc(2048*sizeof(char));
-  mpz_get_str(d1,10,priv->d);
-  mpz_get_str(e1,10,pub->e);
-  mpz_get_str(n1,10,pub->n);
-  char *line_buf = NULL;
-  char *line_buf1 = NULL;
-  char *line_buf2 =NULL;
-  size_t line_buf_size = 0;
-  int res1,res2,res3;
-  if(fichier!=NULL && clefile!=NULL)
-  {
-    while(getline(&line_buf, &line_buf_size, fichier)>=0 && getline(&line_buf1, &line_buf_size, fichier)>=0 && 
-      getline(&line_buf2,&line_buf_size,fichier)>=0)
-       {
-       
-        int test=0;
-        for(int i=0;i<strlen(line_buf)-1;i++)
-        {
-          if(d[i]!=line_buf[i])
-          {
-             test=1;break;
-         }
-        }
-        int test1=0;
-        for(int j=0;j<strlen(line_buf1)-1;j++)
-        {
-          if(line_buf1[j]!=e[j])
-            test1=1;break;
-        }
-        int test2=0;
-        for(int j=0;j<strlen(line_buf2)-1;j++)
-        {
-          if(line_buf2[j]!=n[j])
-            test2=1;break;
-        }
-          if(test==0 && test1==0 && test2==0)
-          { 
-              res1=ecrire_fichier(tmp,d1);
-              res2=ecrire_fichier(tmp,e1);
-              res3=ecrire_fichier(tmp,n1);
-              mpz_set(InfoUser->prive.d,priv->d);
-              mpz_set(InfoUser->prive.n,priv->n);
-              mpz_set(InfoUser->publique.e,pub->e);
-              mpz_set(InfoUser->publique.n,pub->n);
-              if(res1!=0 || res2!=0 || res3!=0)
-              {
-                return ERR_ERCI;
-              }
-            
-          } 
-          else
-          { 
-            int taille=strlen(line_buf)-1;
-            if(line_buf[taille]=='\n')
-            {
-              line_buf[taille]='\0';
-            }
-            taille=strlen(line_buf1)-1;
-            if(line_buf1[taille]=='\n')
-            {
-              line_buf1[taille]='\0';
-            }
-             taille=strlen(line_buf2)-1;
-            if(line_buf2[taille]=='\n')
-            {
-              line_buf2[taille]='\0';
-            }
-             res1=ecrire_fichier(tmp,line_buf);
-             res2=ecrire_fichier(tmp,line_buf1);
-             res3=ecrire_fichier(tmp,line_buf2);
-             if(res1!=0 || res2!=0 || res3!=0)
-             {
-              return ERR_ERCI;
-             }
-          }       
-       }
-  fclose(fichier);
-  fclose(clefile);
-  remove(dossier);
-  rename(tmp,dossier); 
-   return 0;
-  }
-else
-{
-  return ERR_ERCI;
-}}
+
+    char * chemin = malloc (sizeof(char)*(strlen("rsa/")+strlen(InfoUser->email)+strlen("/Cles.txt")+3));
+    char * ligne = malloc (sizeof(char) * 2048);
+
+    sprintf(chemin, "rsa/%s/Cles.txt", InfoUser->email);
+    printf("%s", chemin);
+    FILE * fichier = NULL;
+    fichier = fopen(chemin, "w+");
+
+    if(!fichier) 
+        return ERR_ERCI;
+
+    InfoUser->prive = *priv;
+    InfoUser->publique = *pub;
+
+    mpz_get_str(ligne, 10, InfoUser->prive.d);
+    fprintf(fichier, "%s\n", ligne);
+    mpz_get_str(ligne, 10, InfoUser->publique.e);
+    fprintf(fichier, "%s\n", ligne);
+    mpz_get_str(ligne, 10, InfoUser->publique.n);
+    fprintf(fichier, "%s\n", ligne);
+
+
+
+    free(chemin);
+    free(ligne);
+    fclose(fichier); 
+
+    return 0;
+}
 
 int  ecrire_fichier(char *chemin , char *message)
 {
@@ -334,98 +257,141 @@ int  ecrire_fichier(char *chemin , char *message)
               fclose(fichier);
               return 0;
      }
-     else
-     {
+     
 
       return ERR_ERCI;
-     }
+     
 }
 int envoie_message(mail *m)
 {
         int res1,res2,res3,res4;
-        if(m->signer ==1)
+        if(m->signer ==0)
         {
+          
+
               char *tmp =malloc(strlen(m->titre)*sizeof(char));
               strcpy(tmp,m->titre);
               strcat(tmp,":sig");
-              char *email=malloc((strlen(m->dest_email)+50)*sizeof(char));
-              strcpy(email,"Email:");
+              char *email=malloc((strlen(m->dest_email)+100)*sizeof(char));
+              strcpy(email,"Email : ");
+
               strcat(email,m->dest_email);
-              char *message=malloc((strlen(m->message)+200)*sizeof(char));
-              strcpy(message,"Message Envoyé:");
+              
+            
+              char *message=malloc((strlen(m->message)+300)*sizeof(char));
+              strcpy(message,"Message Envoyé : ");
               strcat(message,m->message);
-              char *signature=malloc((strlen(m->signature)+200)*sizeof(char));
-              strcpy(signature,"Signature Message:");
-              strcat(signature,m->signature);
-              char *chemin=malloc(500*sizeof(char));
+              char *signature = NULL; 
+              if (m-> signature == NULL)
+              {
+                char *signature=malloc(200*sizeof(char));
+                strcpy(signature,"Signature Message : ");
+              }
+              else
+              {
+                signature=malloc((strlen(m->signature)+200)*sizeof(char));
+                strcpy(signature,"Signature Message : ");
+                strcat(signature,m->signature);
+               }
+            
+              
+              char *chemin=malloc(300*sizeof(char));
               strcpy(chemin,"rsa/");
               strcat(chemin,m->env_email);
+             
               strcat(chemin,"/");
               strcat(chemin,"MessageEnvoyer.txt");
+            
               res1=ecrire_fichier(chemin,tmp);
               res2=ecrire_fichier(chemin,email);
               res3=ecrire_fichier(chemin,message);
-              res4=ecrire_fichier(chemin,signature);
-              if(res1!=0 || res2!=0 || res3!=0 || res4!=0)
+              //res4=ecrire_fichier(chemin,signature);
+              if(res1!=0 || res2!=0 || res3!=0 )
               {
                 return ERR_ERCI;
               }
+              free(chemin); 
+              chemin=malloc(300*sizeof(char));
               strcpy(chemin,"rsa/");
               strcat(chemin,m->dest_email);
               strcat(chemin,"/");
               strcat(chemin,"MessageIndesirable.txt");
+              printf(" %s  \n",chemin);
               res1=ecrire_fichier(chemin,tmp);
-              strcpy(email,"Email:");
+              strcpy(email,"Email : ");
               strcat(email,m->env_email);
               res2=ecrire_fichier(chemin,email);
-              strcpy(message,"Message Recu:");
+              strcpy(message,"Message Recu : ");
               strcat(message,m->message);
               res3=ecrire_fichier(chemin,message);
-              strcpy(signature,"Signature Message:");
-              strcat(signature,m->signature);
-              res4=ecrire_fichier(chemin,signature);
-              if(res1!=0 || res2!=0 || res3!=0 || res4!=0)
+              //strcpy(signature,"Signature Message : ");
+           // strcat(signature,m->signature);
+            //res4=ecrire_fichier(chemin,signature);
+
+              if(res1!=0 || res2!=0 || res3!=0  )
               {
                 return ERR_ERCI;
               }
 
         return 0;
         }
-       if(m->signer==0)
+       if(m->signer==1)
        {           
               char *tmp =malloc(strlen(m->titre)*sizeof(char));
               strcpy(tmp,m->titre);
-              strcat(tmp,":Nonsig");
+              strcat(tmp,":sig");
+             
               char *email=malloc((strlen(m->dest_email)+50)*sizeof(char));
-              strcpy(email,"Email:");
+              strcpy(email,"Email : ");
               strcat(email,m->dest_email);
+              
               char *message=malloc((strlen(m->message)+200)*sizeof(char));
-              strcpy(message,"Message Envoyé:");
+              strcpy(message,"Message Envoyé : ");
               strcat(message,m->message);
-              char *chemin=malloc(500*sizeof(char));
+             
+              char *chemin=malloc(300*sizeof(char));
               strcpy(chemin,"rsa/");
               strcat(chemin,m->env_email);
               strcat(chemin,"/");
               strcat(chemin,"MessageEnvoyer.txt");
+              // printf ("hhhhchemin%shhhhhhhhhhh \n",chemin);
+              char *signature = NULL; 
+               if (m-> signature == NULL)
+              {
+                char *signature=malloc(200*sizeof(char));
+                strcpy(signature,"Signature Message : ");
+              }
+              else
+              {
+                signature=malloc((strlen(m->signature)+200)*sizeof(char));
+                strcpy(signature,"Signature Message : ");
+                strcat(signature,m->signature);
+               }
+               printf ("hhhhhhh signature %s\n",signature);
               res1=ecrire_fichier(chemin,tmp);
               res2=ecrire_fichier(chemin,email);
               res3=ecrire_fichier(chemin,message);
-              if(res1!=0 || res2!=0 || res3!=0)
+              res4=ecrire_fichier(chemin,signature);
+              //printf ("%d  %d   %d   %d ", res1 , res2 , res3 , res4);
+              if(res1!=0 || res2!=0 || res3!=0 || res4 != 0)
               {
                 return ERR_ERCI;
               }
+              free(chemin);
+              chemin=malloc(300*sizeof(char));
               strcpy(chemin,"rsa/");
               strcat(chemin,m->dest_email);
               strcat(chemin,"/");
-              strcat(chemin,"MessageInvisible.txt");
+              strcat(chemin,"MessageRecu.txt");
               res1=ecrire_fichier(chemin,tmp);
-              strcpy(email,"Email:");
+              strcpy(email,"Email : ");
               strcat(email,m->env_email);
               res2=ecrire_fichier(chemin,email);
-              strcpy(message,"Message Recu:");
+              strcpy(message,"Message Recu : ");
               strcat(message,m->message);
               res3=ecrire_fichier(chemin,message);
-              if(res1!=0 || res2!=0 || res3!=0)
+              res4=ecrire_fichier(chemin,signature);
+              if(res1!=0 || res2!=0 || res3!=0 || res4 != 0)
               {
                 return ERR_ERCI;
               }
@@ -433,19 +399,23 @@ int envoie_message(mail *m)
        }
        return 0;
 }
-int  stocker_message(char *email , char*  message , char *boite)
+int  stocker_message(char *email , mpz_t  message , char *boite)
 {   
-    int res;
+   int res;
+    char *str=malloc(1024*sizeof(char));
+    mpz_get_str(str,10,message);//conversion_mpz_char
     char dossier[300];
-    strcpy(dossier,"rsa/");
+    strcpy(dossier,"../../RSA-DataSafe/rsa/");
     strcat(dossier,email);
     strcat(dossier,"/");
     strcat(dossier,boite);
-    res=ecrire_fichier(dossier,message);
+    res=ecrire_fichier(dossier,str);
     if(res!=0)
     {
       return ERR_ERCI;
     }
     
-    return 0;
+      return 0;
+    
 }
+
