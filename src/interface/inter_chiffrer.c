d4ed697c08beb4e440a1e0a1c8e42468ad3e08e6
LASSALryma <43700917+LASSALryma@users.noreply.github.com>
2020-05-13 18:17:02 +0200

    Update inter_chiffrer.c

--- src/interface/inter_chiffrer.c
+++ src/interface/inter_chiffrer.c
@@ -119,24 +119,31 @@ void page_chargementC()
   
     gtk_text_buffer_get_end_iter(bufferC,&end);
     chaine= (char*)gtk_text_buffer_get_text(bufferC,&start, &end,FALSE);
-	printf("%s\n", chaine);
     
-     //  #####
+    
+   messageachiff = malloc (sizeof (message));
     codage = malloc (sizeof (message));
-    mpz_init (codage->taille); 
-    messageachiff=conversion_char_mpz(chaine);
-    mpz_init_set_str(codage->nombre ,"ASCCI", 10);
-    mpz_set_ui (codage->taille , mpz_sizeinbase(codage->nombre,2));
-    messagechiff=chiffrement(messageachiff, &utilisateur.publique , codage);
+    mpz_inits (messageachiff->taille , codage->taille , messageachiff->nombre,codage->nombre,NULL ); 
 
+    messageachiff=conversion_char_mpz(chaine );
+    codage=conversion_char_mpz("ASCII" );
    
+    mpz_set_ui (codage->taille , mpz_sizeinbase(codage->nombre,2));
+    gmp_printf("le mess  avant ch %Zd\n",messageachiff->nombre);
+	
+        
+    messagechiff=chiffrement(messageachiff, &utilisateur.publique , codage);
+		gmp_printf("le mess  apres ch %Zd\n",messagechiff->nombre);
 
-   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
   gtk_window_set_transient_for (GTK_WINDOW(window),GTK_WINDOW(MainWindow));
+
   gtk_window_set_position (GTK_WINDOW(window), GTK_WIN_POS_CENTER);
   gtk_window_set_title (GTK_WINDOW(window),"Data Safe");
   gtk_widget_set_size_request(GTK_WIDGET(window) ,500,500);
   vboxCC = gtk_box_new (GTK_ORIENTATION_VERTICAL,50); 
+	
   spinner = gtk_spinner_new ();
 
   	gtk_spinner_start (GTK_SPINNER(spinner));
@@ -166,27 +173,29 @@ int page_resultatC(gpointer * data)
       gtk_container_remove (GTK_CONTAINER(vboxCC),GTK_WIDGET(spinner));
       gtk_widget_hide(GTK_WIDGET(vboxCC)); 
 
-	      GtkWidget * buttonOK = gtk_button_new_with_label ("OK"); 
-	      gtk_widget_set_name (buttonOK,"btnV"); 
-		  gtk_label_set_text  (GTK_LABEL(labelcharc),"Resulat du chiffrement:");
+      GtkWidget * buttonOK = gtk_button_new_with_label ("OK"); 
+      gtk_widget_set_name (buttonOK,"btnV"); 
+	  gtk_label_set_text  (GTK_LABEL(labelcharc),"Resulat du chiffrement:");
 
-		   GtkWidget * Frame ; 
-		   Frame  =gtk_frame_new ("Votre texte chiffré");
-	       gtk_widget_set_name (GTK_WIDGET(Frame),"miniT");	
-        bufferCC= gtk_text_buffer_new (NULL);
-	 	GtkWidget *scrolled_window = gtk_scrolled_window_new (NULL , NULL);
+	   GtkWidget * Frame ; 
+	   Frame  =gtk_frame_new ("Votre texte chiffré");
+       gtk_widget_set_name (GTK_WIDGET(Frame),"miniT");	
+      
+	GtkWidget *scrolled_window = gtk_scrolled_window_new (NULL , NULL);
 	
-  		
-	  //######################"""""""""""""
-		  chaine=conversion_mpz_hexa(messagechiff);
-		  gtk_text_buffer_set_text (bufferCC,chaine, -1);
-			
+  bufferCC= gtk_text_buffer_new (NULL);
+	
+  chaine=malloc(2048*sizeof(char));
+  //chaine=conversion_mpz_char(messagechiff);
+  mpz_get_str(chaine,0,messagechiff->nombre);
+  gtk_text_buffer_set_text (bufferCC,chaine, -1);
+	
 
 
-	  text_viewCC= gtk_text_view_new_with_buffer (bufferCC);
-	  gtk_text_view_set_editable (GTK_TEXT_VIEW(text_viewCC), FALSE);
-	  gtk_container_set_border_width (GTK_CONTAINER (scrolled_window), 10);
-	  gtk_container_add (GTK_CONTAINER (scrolled_window), text_viewCC);
+  text_viewCC= gtk_text_view_new_with_buffer (bufferCC);
+	gtk_text_view_set_editable (GTK_TEXT_VIEW(text_viewCC), FALSE);
+	gtk_container_set_border_width (GTK_CONTAINER (scrolled_window), 10);
+	gtk_container_add (GTK_CONTAINER (scrolled_window), text_viewCC);
     
 	   gtk_container_add (GTK_CONTAINER (Frame), GTK_WIDGET(scrolled_window));
 	   gtk_text_view_set_cursor_visible (GTK_TEXT_VIEW(text_viewCC),FALSE);
