8153079933083ff2b07fe0e502e428a53ebd715a
Lyne <55532663+LyndaHadjab@users.noreply.github.com>
2020-05-16 13:41:24 +0200

    ca fonctionne sans interface mtn c est a l interface d voir comment appeler les fcts

--- src/main.c
+++ src/main.c
@@ -30,36 +30,7 @@ int Lancement()
 int main(int argc , char ** argv)
 {
 	
-    gtk_init(&argc, &argv);
-	
-
-    initialisation();
-	page_ouverture();
-	Page_de_connection();
-	Page_d_inscription();
-	page_Menu();
-	page_Parametre();
-	page_chiffrer();	
-	page_dechiffrer();
-	page_messagerie();
-	page_MessageRecu();
-	page_envoyer_unmsg();
-	page_message_envoyes(); 
-	page_message_indesirable();
-	page_compte();
-	page_securite();
-	
-	CSS();
-
-   g_signal_connect(G_OBJECT(MainWindow), "delete-event", G_CALLBACK(gtk_main_quit), NULL);
-   g_timeout_add (2500,(GSourceFunc)Lancement, NULL);
-   
-   gtk_widget_show_all(MainWindow);
-   gtk_main();
-    
-    return EXIT_SUCCESS;
-    /*
-	cle_publique pub;
+	 cle_publique pub;
 	cle_prive prive;
 	mpz_inits(pub.n, pub.e, prive.n, prive.d, NULL);
 	genere_cle(&pub, &prive, 1024);
@@ -68,7 +39,7 @@ int main(int argc , char ** argv)
     mpz_init(m->nombre);
     mpz_init(m->taille);
     char *machaine=malloc(sizeof(char)*200);
-    strcpy(machaine,"lynda");
+    strcpy(machaine,"hello everyone");
     message *transf;
     transf=conversion_char_mpz(machaine);
   
@@ -132,7 +103,68 @@ int main(int argc , char ** argv)
 	char *sh = conversion_mpz_char(clair);
 	printf("%s\n\n", sh);
 	free(sh);
-	*/
-
 	
+   /* gtk_init(&argc, &argv);
+	
+
+    initialisation();
+	page_ouverture();
+	Page_de_connection();
+	Page_d_inscription();
+	page_Menu();
+	page_Parametre();
+	page_chiffrer();	
+	page_dechiffrer();
+	page_messagerie();
+	page_MessageRecu();
+	page_envoyer_unmsg();
+	page_message_envoyes(); 
+	page_message_indesirable();
+	page_compte();
+	page_securite();
+	
+	CSS();
+
+   g_signal_connect(G_OBJECT(MainWindow), "delete-event", G_CALLBACK(gtk_main_quit), NULL);
+   g_timeout_add (2500,(GSourceFunc)Lancement, NULL);
+   
+   gtk_widget_show_all(MainWindow);
+   gtk_main();
+    
+    return EXIT_SUCCESS;*/
+  
+    
+	/*message * m =conversion_char_mpz("lynda");
+	mpz_out_str(0,10,m->nombre);
+	printf("\n\n");
+	mpz_out_str(0,10,m->taille);
+	 char * chaine;
+	 chaine= conversion_mpz_char(m);
+      printf("\n\n");
+      printf("%s\n",chaine);
+      char * ch=malloc(sizeof(char)*200);
+      strcpy(ch,"2c");
+message *mss;
+     mss=conversion_hexa_mpz(ch);
+     printf("\n");
+     mpz_out_str(0,10,mss->nombre);
+     printf("\n\n");
+     char* mp=conversion_mpz_hexa (mss);
+     printf("%s\n",mp);*/
+  //  char *ch;
+    // ch=conversion_mpz_hexa(m);
+
+    /*cle_publique  publique;
+    cle_prive prive;
+    mpz_inits(publique.e,publique.n,NULL);
+    recupere_cle_publique("test@email.com","user",&publique);
+    recupere_cle_privee("test@email.com","user",&prive);
+    mpz_out_str(0,10,publique.e);
+    printf("\n\n");
+    mpz_out_str(0,10,publique.n);
+    printf("\n\n");
+     mpz_out_str(0,10,prive.d);
+    printf("\n\n");
+    mpz_out_str(0,10,prive.n);*/
+   
 }
