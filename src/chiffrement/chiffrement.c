b2425344922ac2cacca77d1483fda1d1df2bdd6c
Lyne <55532663+LyndaHadjab@users.noreply.github.com>
2020-05-16 13:49:51 +0200

    Update chiffrement.c

--- src/chiffrement/chiffrement.c
+++ src/chiffrement/chiffrement.c
@@ -1,47 +1,34 @@
 #include <gmp.h>
-#include <time.h>
-#include <stdlib.h>
+
 #include "../structure/structure.h"
 #include "../calcul/calcul.h"
-#include "chiffrement.h"
-#include "oaep.h"
-#include "gestion_block.h"
-message *chiffrement(message *m, cle_publique *publique, message *encodage) {
-    
-		
-    mpz_t alea;
-    mpz_init(alea);
+#include "../chiffrement/gestion_block.h"
+#include "oaep_1.h"
+#include "dechiffrement.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-    time_t t; 
-    srand(time(&t));
-    gmp_randstate_t etat;
-    gmp_randinit_default(etat);
-    gmp_randseed_ui(etat, rand());
-    mpz_urandomb(alea, etat, 256);
+message* dechiffrement(message *c, cle_prive *prive) {
 
-    // Code
-    block *b = creer_block_oaep(m, encodage, alea);
-    b = oaep(b, alea);
+	// Initialisation
+	message * m;
+	block * b; 
+	b = creer_block_oaep_1(c);
 
-    mpz_t tmp;
-    mpz_init(tmp);
+	// Code
+	// RSA
+	for (int i = 0; i < b->nb_block; i++) {
+		mpz_powm(b->tab[i], b->tab[i], prive->d, prive->n);
+	}
 
-    for(int i = 0;i<(b->nb_block); i++) {
-        mpz_set(tmp, b->tab[i]);
-        expo_mod(b->tab[i], tmp, publique->e, publique->n);
-    }
-
-    message *k = recupere_message_oaep(b);
-
-    // Clear
-    mpz_clears(tmp,alea,NULL);
-    for(int i = 0;i< (b->nb_block); i++) {
+	// OAEP^(-1)
+	oaep_1(b);
+	m = recupere_message_oaep_1(b);
+	for(int i = 0; i < (b->nb_block); i++) {
         mpz_clear(b->tab[i]);
     }
-    free(b); 
-		
-    // return
-    return k;
+    free(b->tab);
+
+
+	return m;
 }
-
-
