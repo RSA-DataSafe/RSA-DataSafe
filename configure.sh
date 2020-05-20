1a29fa76c2f1cc507c6579e8b7fae2ecc710c36a
Sholde <exshode@gmail.com>
2020-05-20 21:58:18 +0200

    fix

--- configure.sh
+++ configure.sh
@@ -8,13 +8,13 @@ OS=`lowercase \`uname\``
 KERNEL=`uname -r`
 MACH=`uname -m`
 
-if [ "{$OS}" == "windowsnt" ] ; then
+if [ "$OS" = "windowsnt" ] ; then
     echo "Système d'exploitation Windows non supportés"
     echo "Impossible d'installer les dépendances"
-elif [ "{$OS}" == "darwin" ] ; then
+elif [ "$OS" = "darwin" ] ; then
     echo "Système d'exploitation Mac OS non supportés"
     echo "Impossible d'installer les dépendances"
-elif [ `uname` = "Linux" ] ; then
+elif [ "$OS" = "linux" ] ; then
     if [ -f /etc/debian_version ] ; then
         echo "Mise à jour de la liste des paquets"
         sudo apt-get update
